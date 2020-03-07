c305c488bfeb7d28659bbcda3864ae22ddba8bc2
Mark Dorofeev <markdorofeev@Marks-MacBook-Pro.local>
2020-03-08 01:29:49 +0300

    initial commit

---
+++ src/main/java/com/example/transactionacquiringserver/TransactionAcquiringServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionacquiringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAcquiringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAcquiringServerApplication.class, args);
+	}
+
+}
