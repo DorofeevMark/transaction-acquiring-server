8000ba960a22beac5fff55750ddd0b0e20d01275
MarkDorofeev <creekcreek760@gmail.com>
2020-03-31 22:23:01 +0300

    Added logs get request endpoint

--- src/main/java/com/example/transactionacquiringserver/controllers/PaymentController.java
+++ src/main/java/com/example/transactionacquiringserver/controllers/PaymentController.java
@@ -1,6 +1,8 @@
 package com.example.transactionacquiringserver.controllers;
 
 import com.example.transactionacquiringserver.jpa.models.PaymentInfo;
+import com.example.transactionacquiringserver.jpa.models.TransactionLog;
+import com.example.transactionacquiringserver.jpa.repositories.TransactionLogRepository;
 import com.example.transactionacquiringserver.services.PaymentSystemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,17 @@ import java.util.Map;
 public class PaymentController {
     @Autowired
     private PaymentSystemService paymentSystemService;
+    @Autowired
+    private TransactionLogRepository transactionLogRepository;
 
     @PostMapping(path = "/proceed", consumes = "application/json", produces = "application/json")
     public Map<String, String> payment(@RequestBody PaymentInfo paymentInfo) {
         return Collections.singletonMap("response", paymentSystemService.sendRequestToPaymentService(paymentInfo));
+    }
+
+    @GetMapping(path="/getLogs")
+    public @ResponseBody Iterable<TransactionLog> getAllLogs() {
+        return transactionLogRepository.findAll();
     }
 }
 
